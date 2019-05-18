30ab05805a1ed3d1f25012aa626849ee795e53c7
Sam King <kingst@gmail.com>
2019-05-18 13:46:18 -0700

    Simple unit test for error correction logic

--- cardscan/src/test/java/com/getbouncer/cardscan/ExampleUnitTest.java
+++ cardscan/src/test/java/com/getbouncer/cardscan/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.getbouncer.cardscan;
 
+import com.getbouncer.cardscan.ui.ScanActivity;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +12,47 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    @Test
+    public void scanningErrorCorrection_isCorrect() {
+        ScanActivity scanActivity = new ScanActivity();
+        scanActivity.incrementNumber("4242");
+        assertEquals("4242", scanActivity.getNumberResult());
+        scanActivity.incrementNumber("4242");
+        scanActivity.incrementNumber("4242");
+        assertEquals("4242", scanActivity.getNumberResult());
+        scanActivity.incrementNumber("1234");
+        assertEquals("4242", scanActivity.getNumberResult());
+        scanActivity.incrementNumber("1234");
+        assertEquals("4242", scanActivity.getNumberResult());
+        scanActivity.incrementNumber("1234");
+        scanActivity.incrementNumber("1234");
+        assertEquals("1234", scanActivity.getNumberResult());
+
+        Expiry expiry1 = new Expiry();
+        expiry1.string = "0120";
+        expiry1.month = 1;
+        expiry1.year = 2020;
+
+        Expiry expiry2 = new Expiry();
+        expiry2.string = "1121";
+        expiry2.month = 11;
+        expiry2.year = 2021;
+
+        scanActivity.incrementExpiry(expiry1);
+        assertEquals(expiry1, scanActivity.getExpiryResult());
+        scanActivity.incrementExpiry(expiry1);
+        scanActivity.incrementExpiry(expiry1);
+        assertEquals(expiry1, scanActivity.getExpiryResult());
+        scanActivity.incrementExpiry(expiry2);
+        assertEquals(expiry1, scanActivity.getExpiryResult());
+        scanActivity.incrementExpiry(expiry2);
+        assertEquals(expiry1, scanActivity.getExpiryResult());
+        scanActivity.incrementExpiry(expiry2);
+        scanActivity.incrementExpiry(expiry2);
+        assertEquals(expiry2, scanActivity.getExpiryResult());
+
+    }
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
