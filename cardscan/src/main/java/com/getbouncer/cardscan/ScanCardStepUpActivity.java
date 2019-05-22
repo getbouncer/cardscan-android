cd96f8b8b162b83c0207ff7b642f33da20cb89f3
Sam King <kingst@gmail.com>
2019-05-22 15:42:51 -0700

    WIP

--- cardscan/src/main/java/com/getbouncer/cardscan/ScanCardStepUpActivity.java
+++ cardscan/src/main/java/com/getbouncer/cardscan/ScanCardStepUpActivity.java
@@ -7,17 +7,29 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 // WARNING WARNING WARNING DO NOT USE still very much WIP
 public class ScanCardStepUpActivity extends ScanBaseActivity implements View.OnClickListener {
 
     private static final int REQUEST_CODE = 43215;
+    private static final String LAST4 = "last4";
+    private static final String EXPIRY = "expiry";
+    private static final String CARD_NETWORK = "card_network";
 
-    public static void start(Activity activity) {
+    private CreditCard.Network cardNetwork;
+    private String expiry;
+    private String last4;
+
+    public static void start(Activity activity, String last4, String expiry,
+                             CreditCard.Network network) {
         ScanBaseActivity.getMachineLearningThread().warmUp(activity.getApplicationContext());
-        activity.startActivityForResult(new Intent(activity, ScanCardStepUpActivity.class),
-                REQUEST_CODE);
+        Intent intent = new Intent(activity, ScanCardStepUpActivity.class);
+        intent.putExtra(LAST4, last4);
+        intent.putExtra(EXPIRY, expiry);
+        intent.putExtra(CARD_NETWORK, network);
+        activity.startActivityForResult(intent, REQUEST_CODE);
     }
 
     public static boolean isStepUpResult(int requestCode) {
@@ -28,6 +40,28 @@ public class ScanCardStepUpActivity extends ScanBaseActivity implements View.OnC
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_up);
+
+        this.last4 = getIntent().getStringExtra(LAST4);
+        this.expiry = getIntent().getStringExtra(EXPIRY);
+        this.cardNetwork = (CreditCard.Network) getIntent().getSerializableExtra(CARD_NETWORK);
+
+        TextView last4AndExpiry = findViewById(R.id.last4AndExpiry);
+        if (this.cardNetwork == CreditCard.Network.VISA) {
+            last4AndExpiry.setCompoundDrawablesWithIntrinsicBounds(R.drawable.visa,
+                    0, 0, 0);
+        } else if (this.cardNetwork == CreditCard.Network.MASTERCARD) {
+            last4AndExpiry.setCompoundDrawablesWithIntrinsicBounds(R.drawable.mastercard,
+                    0, 0, 0);
+        } else if (this.cardNetwork == CreditCard.Network.AMEX) {
+            last4AndExpiry.setCompoundDrawablesWithIntrinsicBounds(R.drawable.amex,
+                    0, 0, 0);
+        } else if (this.cardNetwork == CreditCard.Network.DISCOVER) {
+            last4AndExpiry.setCompoundDrawablesWithIntrinsicBounds(R.drawable.discover,
+                    0, 0, 0);
+        }
+
+        // XXX FIXME
+        last4AndExpiry.setText(this.last4 + " Exp: " + this.expiry);
 
         findViewById(R.id.scanCardButton).setOnClickListener(this);
         setViewIds(R.id.flashlightButton, R.id.cardRectangle, R.id.shadedBackground, R.id.texture,
