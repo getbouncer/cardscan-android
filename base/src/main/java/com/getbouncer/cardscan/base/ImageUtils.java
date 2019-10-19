28345be3e89981d6e46522d04dbc2ebd1d4dea91
Sam King <kingst@gmail.com>
2019-10-19 03:26:05 -0700

    More cleanup

--- base/src/main/java/com/getbouncer/cardscan/base/ImageUtils.java
+++ base/src/main/java/com/getbouncer/cardscan/base/ImageUtils.java
@@ -5,10 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
-import android.util.Log;
-
-import com.getbouncer.cardscan.base.ssd.DetectedSSDBox;
 
 import java.util.List;
 
