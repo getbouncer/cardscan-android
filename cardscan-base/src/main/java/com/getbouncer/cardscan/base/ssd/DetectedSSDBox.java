ac388dff84de2a839244d213319f7e4e08dbebe2
Steven Liu <steven@getbouncer.com>
2020-02-04 02:22:36 -0800

    sigh

--- cardscan-base/src/main/java/com/getbouncer/cardscan/base/ssd/DetectedSSDBox.java
+++ cardscan-base/src/main/java/com/getbouncer/cardscan/base/ssd/DetectedSSDBox.java
@@ -44,5 +44,24 @@ public class DetectedSSDBox {
             return new JSONObject();
         }
     }
+
+    /**
+     * 0.0.3 of the model has an off-by-one issue w/ the label
+     */
+    public JSONObject toJsonV3() {
+        try {
+            JSONObject result = new JSONObject();
+            result.put("x_min", this.XMin / imageWidth);
+            result.put("y_min", this.YMin / imageHeight);
+            result.put("width", (this.XMax - this.XMin) / imageWidth);
+            result.put("height", (this.YMax - this.YMin) / imageHeight);
+            result.put("label", this.label - 1);
+            result.put("confidence", this.confidence);
+            return result;
+        } catch (JSONException je) {
+            je.printStackTrace();
+            return new JSONObject();
+        }
+    }
 }
 
