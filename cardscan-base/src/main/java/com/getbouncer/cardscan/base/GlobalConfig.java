16025b9d65bcf694968687142cd03e4e88fd4cd5
Adam Wushensky <adam@getbouncer.com>
2020-04-22 12:29:24 -0700

    Remove old version

--- cardscan-base/src/main/java/com/getbouncer/cardscan/base/GlobalConfig.java
+++
@@ -1,5 +0,0 @@
-package com.getbouncer.cardscan.base;
-
-public class GlobalConfig {
-    public static boolean PRINT_TIMING = false;
-}
--- cardscan-base/src/main/java/com/getbouncer/cardscan/base/ssd/Size.kt
+++
@@ -1,6 +0,0 @@
-package com.getbouncer.cardscan.base.ssd
-
-/**
- * A replacement for android.util.Size since that isn't available until API 21.
- */
-data class Size(val width: Int, val height: Int)
