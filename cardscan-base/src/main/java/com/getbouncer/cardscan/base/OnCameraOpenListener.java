1aef0098a84c3a6d0bc9082032e82db39d3690cc
Adam Wushensky <adam@getbouncer.com>
2020-07-17 19:13:44 -0700

    Remove 1.0 code

--- cardscan-base/src/main/java/com/getbouncer/cardscan/base/OnCameraOpenListener.java
+++
@@ -1,8 +0,0 @@
-package com.getbouncer.cardscan.base;
-
-import android.hardware.Camera;
-import androidx.annotation.Nullable;
-
-interface OnCameraOpenListener {
-    void onCameraOpen(@Nullable Camera camera);
-}
