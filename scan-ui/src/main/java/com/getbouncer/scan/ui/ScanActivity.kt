fce823f9f27e1be7f8a9a7e7244495c5062c2234
Adam Wushensky <adam@getbouncer.com>
2020-10-07 13:57:31 -0700

    Fix lint

--- scan-ui/src/main/java/com/getbouncer/scan/ui/ScanActivity.kt
+++ scan-ui/src/main/java/com/getbouncer/scan/ui/ScanActivity.kt
@@ -157,11 +157,11 @@ abstract class ScanActivity : AppCompatActivity(), CoroutineScope {
             @Suppress("deprecation")
             window.decorView.apply {
                 systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             }
         }
     }
