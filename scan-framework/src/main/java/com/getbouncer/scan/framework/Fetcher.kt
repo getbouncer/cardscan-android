1a27613a65e7d26701852cae4cff94126c413d43
Adam Wushensky <adam.wushensky@protonmail.com>
2021-05-17 16:56:59 -0700

    Improve error handling around cache calculation

--- scan-framework/src/main/java/com/getbouncer/scan/framework/Fetcher.kt
+++ scan-framework/src/main/java/com/getbouncer/scan/framework/Fetcher.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileInputStream
+import java.io.FileNotFoundException
 import java.io.IOException
 import java.lang.Exception
 import java.net.URL
@@ -60,7 +61,7 @@ sealed class FetchedData(
                     modelClass = modelClass,
                     modelFrameworkVersion = modelFrameworkVersion,
                     modelVersion = meta.modelVersion,
-                    modelHash = meta.modelFile?.let { runBlocking { calculateHash(it, meta.hashAlgorithm) } },
+                    modelHash = meta.modelFile?.let { runBlocking { try { calculateHash(it, meta.hashAlgorithm) } catch (t: Throwable) { null } } },
                     modelHashAlgorithm = meta.hashAlgorithm,
                     file = meta.modelFile
                 )
@@ -498,13 +499,16 @@ abstract class UpdatingModelWebFetcher(context: Context) : SignedUrlModelWebFetc
      * Delete all files in cache that are not the recently downloaded file.
      */
     override suspend fun cleanUpPostDownload(downloadedFile: File) = withContext(Dispatchers.IO) {
-        getCacheFolder()
-            .listFiles()
-            ?.filter { it != downloadedFile && calculateHash(it, defaultModelHashAlgorithm) != defaultModelHash }
-            ?.sortedByDescending { it.lastModified() }
-            ?.filterIndexed { index, _ -> index > CACHE_MODEL_MAX_COUNT }
-            ?.forEach { it.delete() }
-            .let { }
+        try {
+            getCacheFolder()
+                .listFiles()
+                ?.filter { it != downloadedFile && calculateHash(it, defaultModelHashAlgorithm) != defaultModelHash }
+                ?.sortedByDescending { it.lastModified() }
+                ?.filterIndexed { index, _ -> index > CACHE_MODEL_MAX_COUNT }
+                ?.forEach { it.delete() }
+        } catch (t: Throwable) {
+            Log.e(Config.logTag, "Error cleaning up post download", t)
+        }.let { }
     }
 
     /**
@@ -512,10 +516,15 @@ abstract class UpdatingModelWebFetcher(context: Context) : SignedUrlModelWebFetc
      */
     private suspend fun getMatchingFile(hash: String, hashAlgorithm: String): File? =
         withContext(Dispatchers.IO) {
-            getCacheFolder()
-                .listFiles()
-                ?.sortedByDescending { it.lastModified() }
-                ?.firstOrNull { calculateHash(it, hashAlgorithm) == hash }
+            try {
+                getCacheFolder()
+                    .listFiles()
+                    ?.sortedByDescending { it.lastModified() }
+                    ?.firstOrNull { calculateHash(it, hashAlgorithm) == hash }
+            } catch (t: Throwable) {
+                Log.e(Config.logTag, "Unable to get matching file", t)
+                null
+            }
         }
 
     /**
@@ -646,6 +655,7 @@ abstract class UpdatingResourceFetcher(context: Context) : UpdatingModelWebFetch
 /**
  * Determine if a [File] matches the expected [hash].
  */
+@Throws(IOException::class, NoSuchAlgorithmException::class)
 private suspend fun fileMatchesHash(localFile: File, hash: String, hashAlgorithm: String) = try {
     hash == calculateHash(localFile, hashAlgorithm)
 } catch (t: Throwable) {
